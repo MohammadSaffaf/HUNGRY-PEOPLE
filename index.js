92fbcea21c6c9bbe65c3afb85c124021c43b9101
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-12-20 22:41:14 +0100

    views

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.set('view engine' , 'ejs')
 app.get('/' ,(req,res) =>{
     res.render('index',{title:'Home/About'})
 })
+app.get('/team' ,(req,res) =>{
+    res.render('team',{title:'Team'})
+})
+app.get('/booking' ,(req,res) =>{
+    res.render('booking',{title:'Booking'})
+})
+app.get('/GALLERY' ,(req,res) =>{
+    res.render('GALLERY',{title:'GALLERY'})
+})
+app.get('/MENU' ,(req,res) =>{
+    res.render('MENU',{title:'MENU'})
+})
 
 
 
